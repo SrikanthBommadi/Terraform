e7cbc30928863e0b39ce8b8b3e53e4edbb63f3be
SrikanthBommadi <bommadisrikanth1@gmail.com>
2025-02-17 23:34:07 -0500

    changes look shiva

--- expense -terraform/60-backend/backend.sh
+++ expense -terraform/60-backend/backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dnf install ansible -y
-ansible-pull -i localhost, -U  main.yaml -e COMPONENT=backend
+ansible-pull -i localhost, -U https://github.com/SrikanthBommadi/Terraform/tree/main/ansible.terraform main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
